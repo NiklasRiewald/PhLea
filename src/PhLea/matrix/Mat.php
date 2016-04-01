03c181a7ad623bb22eaf9d7eb556a68c488f7f33
nriewald <niklas.riewald@gmail.com>
2016-04-01 23:45:37 +0200

    add phpDoc // better comments

--- src/PhLea/matrix/Mat.php
+++ src/PhLea/matrix/Mat.php
@@ -9,10 +9,15 @@ class Mat
     protected $columns;
     protected $data;
 
+    /**
+     * @param int $columns
+     * @param int $rows
+     * @param \SplFixedArray $data
+     */
     public function __construct($columns, $rows, \SplFixedArray $data = null)
     {
         $this->size = $rows * $columns;
-        if($data == null) {
+        if ($data == null) {
             $this->data = new \SplFixedArray($this->size);
         } else {
             $this->data = $data;
@@ -22,18 +27,26 @@ class Mat
         $this->columns = $columns;
     }
 
-    public function sum(Mat $matB)
+    /**
+     * @param Mat mat
+     * @return Mat
+     */
+    public function sum(Mat $mat)
     {
         $resultMat = new Mat($this->columns, $this->rows);
         for ($y = 0; $y < $this->rows; $y++) {
             for ($x = 0; $x < $this->columns; $x++) {
                 $index = $this->getArrayIndex($x, $y);
-                $resultMat->setAtIndex($index, $this->data[$index] + $matB->getAtIndex($index));
+                $resultMat->setAtIndex($index, $this->data[$index] + $mat->getAtIndex($index));
             }
         }
         return $resultMat;
     }
 
+    /**
+     * @param Mat mat
+     * @return Mat
+     */
     public function dot(Mat $matB)
     {
         $columnsB = $matB->getColumns();
@@ -50,6 +63,9 @@ class Mat
         return $resultMat;
     }
 
+    /**
+     * @return Mat
+     */
     public function getRealTranspose()
     {
         $transposedMat = new Mat($this->rows, $this->columns);
@@ -61,18 +77,30 @@ class Mat
         return $transposedMat;
     }
 
+    /**
+     * @return TransposedMat
+     */
     public function getTranspose()
     {
         return new TransposedMat($this);
     }
 
-    public function getInverse() {
+    /**
+     * @return Mat
+     */
+    public function getInverse()
+    {
         $inverse = clone $this;
         return $inverse->inverse();
     }
 
-    //inverse matrix using LU decomposition
-    //look at http://www.cl.cam.ac.uk/teaching/1314/NumMethods/supporting/mcmaster-kiruba-ludecomp.pdf for explanation
+    /**
+     * Inverse matrix using LU decomposition
+     * Look at http://www.cl.cam.ac.uk/teaching/1314/NumMethods/supporting/mcmaster-kiruba-ludecomp.pdf for an explanation
+     * Returns true if the inversion is successful and false if the matrix is singular or not square
+     *
+     * @return bool
+     */
     public function inverse()
     {
         if ($this->rows != $this->columns) {
@@ -82,11 +110,11 @@ class Mat
         $resultMat = new Mat($this->columns, $this->rows);
 
         $this->decomposeIntoLU();
-        $rowsMinusOne = $this->rows-1;
+        $rowsMinusOne = $this->rows - 1;
 
         for ($y = 0; $y < $this->rows; $y++) {
 
-            //initialising b
+            //initialising b (the y-th unit vector)
             $b = new \SplFixedArray($this->rows);
             for ($x = 0; $x < $this->rows; $x++) {
                 if ($y == $x) {
@@ -109,15 +137,16 @@ class Mat
 
             //solving Uw = z
             $z[$rowsMinusOne] = $z[$rowsMinusOne] / $this->get($rowsMinusOne, $rowsMinusOne);
-            for($k = $rowsMinusOne - 1; $k >= 0; $k--) {
+            for ($k = $rowsMinusOne - 1; $k >= 0; $k--) {
                 $s = 0;
-                for($x = $k + 1; $x < $this->rows; $x++) {
+                for ($x = $k + 1; $x < $this->rows; $x++) {
                     $s += $this->get($x, $k) * $z[$x];
                 }
                 $z[$k] = ($z[$k] - $s) / $this->get($k, $k);
             }
 
-            for($x = 0; $x < $this->rows; $x++) {
+            //copying data into result matrix
+            for ($x = 0; $x < $this->rows; $x++) {
                 $resultMat->set($y, $x, $z[$x]);
             }
         }
@@ -125,7 +154,8 @@ class Mat
         return true;
     }
 
-    public function decomposeIntoLU() {
+    public function decomposeIntoLU()
+    {
         $rowsMinusOne = $this->rows - 1;
         for ($k = 0; $k < $rowsMinusOne; $k++) {
             for ($y = $k + 1; $y < $this->rows; $y++) {
@@ -138,41 +168,73 @@ class Mat
         }
     }
 
+    /**
+     * @param int $x
+     * @param int $y
+     * @return float
+     */
     public function get($x, $y)
     {
         return $this->data[$y * $this->columns + $x];
     }
 
+    /**
+     * @param int $i
+     * @return float
+     */
     public function getAtIndex($i)
     {
         return $this->data[$i];
     }
 
+    /**
+     * @param int $x
+     * @param int $y
+     * @return int
+     */
     public function getArrayIndex($x, $y)
     {
         return $y * $this->columns + $x;
     }
 
+    /**
+     * @param int $x
+     * @param int $y
+     * @param float $value
+     */
     public function set($x, $y, $value)
     {
         $this->data[$y * $this->columns + $x] = $value;
     }
 
+    /**
+     * @param int $i
+     * @param float $value
+     */
     public function setAtIndex($i, $value)
     {
         $this->data[$i] = $value;
     }
 
+    /**
+     * @return int
+     */
     public function getRows()
     {
         return $this->rows;
     }
 
+    /**
+     * @return int
+     */
     public function getColumns()
     {
         return $this->columns;
     }
 
+    /**
+     * @return int
+     */
     public function getSize()
     {
         return $this->data->getSize();
