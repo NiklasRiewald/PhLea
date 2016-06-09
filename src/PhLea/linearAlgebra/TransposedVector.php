cb52ce409856aa4a5a0150a7e1d6720dc0ed0b16
nriewald <niklas.riewald@gmail.com>
2016-06-09 22:26:38 +0200

    new class: TransposedVector

---
+++ src/PhLea/linearAlgebra/TransposedVector.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace PhLea\linearAlgebra;
+
+
+class TransposedVector extends Mat
+{
+    public function __construct($size, \SplFixedArray $data = null)
+    {
+        parent::__construct($size, 1, $data);
+    }
+
+    public function setValue($x, $value)
+    {
+        $this->set($x, 1, $value);
+    }
+}
