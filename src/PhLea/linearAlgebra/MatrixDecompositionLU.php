97c98e72d4410b50e1ce9dc9c10ba3efc942891b
nriewald <niklas.riewald@gmail.com>
2016-07-05 22:27:50 +0200

    use pivoting and store permutation matrix when calculating lu decomposition

--- src/PhLea/linearAlgebra/MatrixDecompositionLU.php
+++ src/PhLea/linearAlgebra/MatrixDecompositionLU.php
@@ -6,13 +6,28 @@ namespace PhLea\linearAlgebra;
 
 class MatrixDecompositionLU
 {
+    /*@var Mat */
+    private $permutationMatrix;
 
     public function decompose(Mat $mat)
     {
+        $this->permutationMatrix = Mat::getIdentityMatrix($mat->getRows());
         $rows = $mat->getRows();
         $rowsMinusOne = $rows - 1;
         for ($k = 0; $k < $rowsMinusOne; $k++) {
             for ($y = $k + 1; $y < $rows; $y++) {
+
+                $pivot = $k;
+                for ($i = $k + 1; $i < $rows; $i++) {
+                    if (abs($mat->get($k, $i)) > abs($mat->get($k, $pivot))) {
+                        $pivot = $i;
+                    }
+                }
+                if ($pivot != $k) {
+                    $mat->swapRows($pivot, $k);
+                    $this->permutationMatrix->swapRows($pivot, $k);
+                }
+
                 $mat->set($k, $y, $mat->get($k, $y) / $mat->get($k, $k));
 
                 for ($x = $k + 1; $x < $rows; $x++) {
@@ -20,5 +35,10 @@ class MatrixDecompositionLU
                 }
             }
         }
+    }
+
+    public function getPermutationMatrix()
+    {
+        return $this->permutationMatrix;
     }
 }
--- tests/PhLeaTest/linearAlgebra/MatrixDecompositionLUTest.php
+++ tests/PhLeaTest/linearAlgebra/MatrixDecompositionLUTest.php
@@ -24,6 +24,41 @@ class MatrixDecompositionLUTest extends \PHPUnit_Framework_TestCase
             0.375, 0.44, 5.4000000000000004
         )));
 
+        $permutationMatrixExpected = new Mat(3, 3, \SplFixedArray::fromArray(array(
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1
+        )));
+
         $this->assertEquals($matExpected, $mat);
+        $this->assertEquals($permutationMatrixExpected, $decomposition->getPermutationMatrix());
+    }
+
+    public function testDecomposeWherePivotingIsNeeded()
+    {
+        $mat = new Mat(3, 3, \SplFixedArray::fromArray(array(
+            1, 4, 5,
+            -6, 4, 2,
+            -4, -2, -1,
+        )));
+
+        $factory = new LinearAlgebraFactory();
+        $decomposition = $factory->getInstanceOfMatrixDecompositionLU();
+        $decomposition->decompose($mat);
+
+        $matExpected = new Mat(3, 3, \SplFixedArray::fromArray(array(
+            -6, 4, 2,
+            -0.167, 4.667, 5.333,
+            0.667, -1, 3
+        )));
+
+        $permutationMatrixExpected = new Mat(3, 3, \SplFixedArray::fromArray(array(
+            0, 1, 0,
+            1, 0, 0,
+            0, 0, 1
+        )));
+
+        $this->assertEquals($matExpected, $mat, '', 0.001);
+        $this->assertEquals($permutationMatrixExpected, $decomposition->getPermutationMatrix());
     }
 }
